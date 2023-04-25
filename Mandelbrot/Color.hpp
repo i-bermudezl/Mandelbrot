8e8e649281fb93f76c671cd68fef68049fd643ab
Isaac David <i.bermudezl@uniandes.edu.co>
2023-04-25 10:46:52 -0500

    Initial commit

---
+++ Mandelbrot/Color.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <cstdint>
+
+struct R8G8B8A8
+{
+	uint8_t r;
+	uint8_t g;
+	uint8_t b;
+	uint8_t a;
+};
+
+struct RGBA64f
+{
+	double r;
+	double g;
+	double b;
+	double a;
+};
